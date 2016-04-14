0eaac998593a6ee10b25692b4f6d26092a9e1355
jerrycjw <YOUR EMAIL ADDRESS>
2016-04-13 23:13:14 -0700

    create authentication tables and access control methods

--- applications/welcome/models/access_control.py
+++ applications/welcome/models/access_control.py
@@ -1,13 +1,61 @@
+#########################################################################
+## This file defines access control methods, including verification, setting and deleting.
+## - access_view can view only.
+## - access_setting can change settings, and view variables/output, but cannot change procedure code.
+## - access_manage can do everything the main user can do.
+#########################################################################
 
-#return if the user has authority to view device
+
+#########################################################################
+# Verification methods
+#########################################################################
+
+# return if the user has authority to view device
 def access_view(user_id, device_token):
-    return auth.has_permission(name="view",group_id=device_token,user_id=user_id)
-#return if the user has authority to configure device
+    return auth.has_permission(name="view", group_id=(device_token + "_" + user_id), user_id=user_id)
+
+
+# return if the user has authority to configure device
 def access_setting(user_id, device_token):
-    return auth.has_permission(name="setting", group_id=device_token, user_id=user_id)
-#return if the user has authority to share the device with others
+    return auth.has_permission(name="setting", group_id=(device_token + "_" + user_id), user_id=user_id)
+
+
+# return if the user has authority to share the device with others
 def access_manage(user_id, share_user_email, device_token):
-    return auth.has_permission(name="manage", group_id=device_token, user_id=user_id)
-#set authority relation between user and device
-def access_set_authority(user_id, device_token, auth_level, role):
-    return auth.add_permission(name=role, group_id=(device_token+auth_level), user_id=user_id)
+    return auth.has_permission(name="manage", group_id=(device_token + "_" + user_id), user_id=user_id)
+
+
+#########################################################################
+# Setting methods
+#########################################################################
+
+# give user_id "view" access to "device_token"
+def set_access_view(user_id, device_token):
+    return auth.add_permission(name="view", group_id=(device_token + "_" + user_id), user_id=user_id)
+
+
+# give user_id "setting" access to "device_token"
+def set_access_setting(user_id, device_token):
+    return auth.add_permission(name="setting", group_id=(device_token + "_" + user_id), user_id=user_id)
+
+
+# give user_id "manage" access to "device_token"
+def set_access_manage(user_id, device_token):
+    return auth.add_permission(name="manage", group_id=(device_token + "_" + user_id), user_id=user_id)
+
+
+#########################################################################
+# deleting methods
+#########################################################################
+
+#delete user_id "view" access to "device_token"
+def del_access_view(user_id, device_token):
+    return auth.del_permission(name="view", group_id=(device_token + "_" + user_id))
+
+#delete user_id "setting" access to "device_token"
+def del_access_setting(user_id, device_token):
+    return auth.del_permission(name="setting", group_id=(device_token + "_" + user_id), user_id=user_id)
+
+#delete user_id "manage" access to "device_token"
+def del_access_manage(user_id, device_token):
+    return auth.del_permission(name="manage", group_id=(device_token + "_" + user_id), user_id=user_id)
--- applications/welcome/models/tables.py
+++ applications/welcome/models/tables.py
@@ -26,7 +26,7 @@ db.define_table('logs',
                 Field('device_id'),
                 Field('timestamp', 'datetime', default=datetime.datetime.utcnow()),
                 Field('module'),
-                Field('level', 'integer'), #  int, 0 = most important.
+                Field('level', 'integer'),  # int, 0 = most important.
                 Field('message', 'text'),
                 )
 
@@ -34,8 +34,8 @@ db.define_table('output',
                 Field('device_id'),
                 Field('timestamp', 'datetime', default=datetime.datetime.utcnow()),
                 Field('module'),
-                Field('name'), # Name of variable
-                Field('value', 'text'), # Json, short please
+                Field('name'),  # Name of variable
+                Field('value', 'text'),  # Json, short please
                 )
 
 db.define_table('values',
@@ -45,3 +45,60 @@ db.define_table('values',
                 Field('name'),  # Name of variable
                 Field('value', 'text'),  # Json, short please
                 )
+
+#########################################################################
+#These correspond to authentication tables.
+#########################################################################
+
+from gluon.tools import Auth, Service, PluginManager
+
+auth = Auth(db)
+
+## after auth = Auth(db)
+
+db.define_table(
+    auth.settings.table_user_name,
+    Field('first_name', length=128, default=''),
+    Field('last_name', length=128, default=''),
+    Field('email', length=128, default='', unique=True),  # required
+    Field('password', 'password', length=512,  # required
+          readable=False, label='Password'),
+    Field('address'),
+    Field('city'),
+    Field('phone'),
+    Field('registration_key', length=512,  # required
+          writable=False, readable=False, default=''),
+    Field('reset_password_key', length=512,  # required
+          writable=False, readable=False, default=''),
+    Field('registration_id', length=512,  # required
+          writable=False, readable=False, default=''))
+
+custom_auth_table = db[auth.settings.table_user_name]  # get the custom_auth_table
+custom_auth_table.first_name.requires = IS_NOT_EMPTY(error_message=auth.messages.is_empty)
+custom_auth_table.last_name.requires = IS_NOT_EMPTY(error_message=auth.messages.is_empty)
+custom_auth_table.password.requires = [IS_STRONG(), CRYPT()]
+custom_auth_table.email.requires = [
+    IS_EMAIL(error_message=auth.messages.invalid_email),
+    IS_NOT_IN_DB(db, custom_auth_table.email)]
+
+auth.settings.table_user = custom_auth_table  # tell auth to use custom_auth_table
+
+
+## before auth.define_tables()
+auth.define_tables()
+
+#########################################################################
+#These conrespond to device table
+#########################################################################
+
+db.define_table(
+    "Device_Table",
+    Field("device_id", length=128, default=''),  # required
+    Field("owner_email", length=128, default=''),  # required
+    Field("name", length=128, default=''),  # required
+    Field("description", length=512, default='')
+)
+client_table = db["Device_Table"]  # get the custom_auth_permission_table
+client_table.device_id.requires = IS_NOT_EMPTY(error_message=auth.messages.is_empty)
+client_table.owner_email.requires = IS_NOT_EMPTY(error_message=auth.messages.is_empty)
+client_table.name.requires = IS_NOT_EMPTY(error_message=auth.messages.is_empty)
