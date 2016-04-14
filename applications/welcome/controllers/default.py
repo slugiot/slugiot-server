12c9faae2db9538e03ff53b6c47abf25fe12651d
LIXiao <xli111@ucsc.edu>
2016-04-14 11:23:07 -0700

    add the backend test controller for editor

--- applications/welcome/controllers/default.py
+++ applications/welcome/controllers/default.py
@@ -57,4 +57,54 @@ def call():
     """
     return service()
 
+from datetime import datetime
 
+"""
+inorder to test we need to define a table
+
+db.define_table('coding',
+            Field('procedures', 'text'),
+            Field('times','datetime')
+            )
+"""
+
+def edit():
+    return dict()
+
+def edit_procedure():
+    """ return json data to show the editor content """
+    # Load json only if it is ajax edited...
+    preferences={'theme':'web2py', 'editor': 'default', 'closetag': 'true', 'codefolding': 'false', 'tabwidth':'4', 'indentwithtabs':'false', 'linenumbers':'true', 'highlightline':'true'}
+    codeId = request.vars.code
+    data =  db(db.coding.id == co).select(db.coding.procedures).first().procedures
+    file_details = dict(
+                    editor_settings=preferences,
+                    data=data,
+                    id=codeId,
+                    )
+    plain_html = response.render('default/edit_js.html', file_details)
+    file_details['plain_html'] = plain_html
+    return response.json(file_details)
+
+
+def create():
+    """
+    create a new procedure
+    """
+    now = datetime.utcnow()
+    code = "# enter your new procedure in the following" + now.strftime("%Y-%m-%d %H:%M:%S")
+    db.coding.insert(procedures=code, times=now)
+    result = 'ok'
+    return dict(result = now)
+
+def select():
+    """
+    search for the exit procedure in the database
+    :return:
+    """
+    row= db(db.coding).select().first()
+    if row is not None:
+        rows = db(db.coding).select()
+        code_list = [{'id': r.id, 'procedures': r.procedures, 'times': r.times} for r in rows]
+        return dict(exit = 1, code_list = code_list)
+    return dict(exit = 0)
