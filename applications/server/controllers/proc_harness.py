8f22d4c6b64f9d94dd19da7acd96566e5ceaeca3
Allison Hume <arhume@ucsc.edu>
2016-04-19 10:01:02 -0700

    should be relatively stable - need error checking and have a few questions

--- applications/server/controllers/proc_harness.py
+++ applications/server/controllers/proc_harness.py
@@ -31,15 +31,16 @@ def get_procedures_for_user(user_id):
     return procedure_ids
 
 # True for stable gets last stable, False gets most recent stable or not
+# Do we have to worry about stuff coming in at the same time? Currently a bug
 def get_procedure_data(procedure_id, stable):
     max = revisions_table.last_update.max()
     if stable:
-        date = db(revisions_table.procedure_id == procedure_id,
-                  revisions_table.stable_version == stable).select(max)
+        date = db((revisions_table.procedure_id == procedure_id) &
+                                      (revisions_table.stable_version == stable)).select(max).first()[max]
     else:
-        date = db(revisions_table.procedure_id == procedure_id).select(max)
-    return db(revisions_table.procedure_id == procedure_id,
-                revisions_table.last_update == date).select(revisions_table.procedure_data).first().procedure_data
+        date = db(revisions_table.procedure_id == procedure_id).select(max).first()[max]
+    return db((revisions_table.procedure_id == procedure_id) &
+              (revisions_table.last_update == date)).select(revisions_table.procedure_data).first().procedure_data
 
 # True for stable is save stable, False is save temp
 # when save is stable, flush all temp versions
@@ -60,14 +61,13 @@ def save(procedure_id, procedure_data, stable):
 
 # called in response to client request for procedure info
 # returns a dictionary of the format {procedure_id, last_update_stable}
-# when does a procedure get registered to a device? should this happen in first function??
 def get_procedure_status(device_id):
     # 1. Get all procedure_ids for the device_id
-    procedure_ids = db(db.devices.device_id == device_id,
-                       db.devices.user_id == proc_table.user_id).select(proc_table.procedure_id)
+    procedure_ids = db((db.devices.device_id == device_id) &
+                       (db.devices.user_id == proc_table.user_id)).select(proc_table.procedure_id)
     # 2. build dictionary containing last_update_stable date for each procedure_id
     procedure_info = {}
     for proc in procedure_ids:
-        procedure_info[proc] = get_procedure_data(proc, True)
+        procedure_info[proc.procedure_id] = get_procedure_data(proc, True)
     # 3. return dictionary
     return procedure_info
