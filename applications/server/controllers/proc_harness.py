f10f8567428d0920dbff103d2d9b3d048e6b0122
Allison Hume <arhume@ucsc.edu>
2016-04-19 12:26:43 -0700

    added another function for procedure harness API

--- applications/server/controllers/proc_harness.py
+++ applications/server/controllers/proc_harness.py
@@ -36,7 +36,7 @@ def get_procedure_data(procedure_id, stable):
     max = revisions_table.last_update.max()
     if stable:
         date = db((revisions_table.procedure_id == procedure_id) &
-                                      (revisions_table.stable_version == stable)).select(max).first()[max]
+                  (revisions_table.stable_version == stable)).select(max).first()[max]
     else:
         date = db(revisions_table.procedure_id == procedure_id).select(max).first()[max]
     return db((revisions_table.procedure_id == procedure_id) &
@@ -52,8 +52,6 @@ def save(procedure_id, procedure_data, stable):
                            stable_version = stable)
     #flush temp versions for stable save
     if stable:
-        db((db[table].uuid == item.uuid) &
-           (db[table].id != item.id)).delete()
         db((revisions_table.procedure_id == procedure_id) &
            (revisions_table.stable_version == False)).delete()
 
@@ -63,11 +61,23 @@ def save(procedure_id, procedure_data, stable):
 # returns a dictionary of the format {procedure_id, last_update_stable}
 def get_procedure_status(device_id):
     # 1. Get all procedure_ids for the device_id
-    procedure_ids = db((db.devices.device_id == device_id) &
-                       (db.devices.user_id == proc_table.user_id)).select(proc_table.procedure_id)
+    procedure_ids = db((devices.device_id == device_id) &
+                       (devices.user_id == proc_table.user_id)).select(proc_table.procedure_id)
     # 2. build dictionary containing last_update_stable date for each procedure_id
     procedure_info = {}
     for proc in procedure_ids:
         procedure_info[proc.procedure_id] = get_procedure_data(proc, True)
     # 3. return dictionary
     return procedure_info
+
+# called in response to client request for a procedure update
+# client provides list of procedure_ids that must be updated
+def get_procedure_update(procedure_id_lst):
+    # need to add each record to a json object and send it
+    for proc_id in procedure_id_lst:
+        record = db(revisions_table.procedure_id == proc_table.proc_id).select(proc_table.procedure_id,
+                                                                                 proc_table.user_id,
+                                                                                 revisions_table.last_update,
+                                                                                 proc_table.procedure_name,
+                                                                                 revisions_table.procedure_data).first()
+    return record
