de8cf5f656209926336a35cbdd1623f2f38c88ab
Allison Hume <arhume@ucsc.edu>
2016-04-19 12:54:03 -0700

    added docstrings

--- applications/server/controllers/proc_harness.py
+++ applications/server/controllers/proc_harness.py
@@ -14,66 +14,127 @@ revisions_table = db.procedure_revisions
 
 ####### API FOR EDITOR TEAM ##########
 
-# return procedure_id to be used for a new procedure being created
-# note - currently this function does NOT implicitly save the procedure_id in the table
 def create_procedure(procedure_name, user_id):
-    return proc_table.insert(user_id = user_id,
-                                name = procedure_name)
+    """
+    This function should be called when a new procedure is created in the editor.
 
-# return list procedure_id
-# alternatively: return db(proc_table.user_id == user_id).select(proc_table.procedure_id)
-# second option returns Rows object not list
+    :param procedure_name: Name of the new procedure
+    :type procedure_name: str
+    :param user_id: User id associated with the account that created the procedure
+    :type user_id: str
+    :return: ID associated with new procedure (procedure_id in revisions table)
+    :rtype: long
+    """
+    return proc_table.insert(user_id = user_id, name = procedure_name)
+
+
 def get_procedures_for_user(user_id):
+    """
+    This function returns all procedure IDs that are associated with a given user
+
+    :param user_id: User id associated with the account that is trying to access their procedures
+    :type user_id: str
+    :return: List of procedure IDs associated with user_id
+    :rtype:
+    """
+
+    # Get all relevant records for user_id
     records = db(proc_table.user_id == user_id).select()
+
+    # Create list of procedure IDs from records
     procedure_ids = []
     for row in records:
         procedure_ids.append(row.id)
     return procedure_ids
 
-# True for stable gets last stable, False gets most recent stable or not
+
 # Do we have to worry about stuff coming in at the same time? Currently a bug
 def get_procedure_data(procedure_id, stable):
+    """
+    Returns actual code that corresponds to a given procedure ID.
+    Returns either the most recent stable version or the most recent absolute version.
+
+    :param procedure_id: Procedure ID for which code should be fetched
+    :type procedure_id: long
+    :param stable: Flag that determines whether the code returned should be most recent stable or just the most recent
+    :type stable: bool
+    :return: Data stored for the procedure
+    :rtype: str
+    """
+
+    # Get the most recent date, either stable or absolute
     max = revisions_table.last_update.max()
     if stable:
         date = db((revisions_table.procedure_id == procedure_id) &
                   (revisions_table.stable_version == stable)).select(max).first()[max]
     else:
         date = db(revisions_table.procedure_id == procedure_id).select(max).first()[max]
+
+    # Return the data corresponding the procedure ID and determined date
     return db((revisions_table.procedure_id == procedure_id) &
               (revisions_table.last_update == date)).select(revisions_table.procedure_data).first().procedure_data
 
-# True for stable is save stable, False is save temp
-# when save is stable, flush all temp versions
+
 def save(procedure_id, procedure_data, stable):
-    # insert new record
+    """
+    Save code corresponding to a procedure ID as either a stable version or a temporary version
+
+    :param procedure_id: Procedure ID for which code should be fetched
+    :type procedure_id: long
+    :param procedure_data: Code that should be saved
+    :type procedure_data: str
+    :param stable: Flag that determines whether the code should be sent to client on next request or not
+    :type stable: bool
+    """
+
+    # Insert new record to revisions table
     revisions_table.insert(procedure_id = procedure_id,
                            procedure_data = procedure_data,
                            last_update = datetime.datetime.utcnow(),
                            stable_version = stable)
-    #flush temp versions for stable save
+
+    # Only keep temporary revisions until next stable revision comes in
+    # Clean up old temporary revisions upon stable save
     if stable:
         db((revisions_table.procedure_id == procedure_id) &
            (revisions_table.stable_version == False)).delete()
 
+
 ####### API FOR PROCEDURE HARNESS TEAM ##########
 
-# called in response to client request for procedure info
-# returns a dictionary of the format {procedure_id, last_update_stable}
+
 def get_procedure_status(device_id):
-    # 1. Get all procedure_ids for the device_id
+    """
+    Called upon client request for procedure info
+
+    :param device_id: Device id for which the procedure sync is being performed
+    :type device_id: long
+    :return Dict of the format {procedure_id: last_update_stable}
+                    that specifies the last save time for each procedure associated with the device
+    :rtype:
+    """
+
+    # Get all procedure_ids for the device_id
     procedure_ids = db((devices.device_id == device_id) &
                        (devices.user_id == proc_table.user_id)).select(proc_table.procedure_id)
-    # 2. build dictionary containing last_update_stable date for each procedure_id
+
+    # Build dictionary containing last_update_stable date for each procedure_id
     procedure_info = {}
     for proc in procedure_ids:
         procedure_info[proc.procedure_id] = get_procedure_data(proc, True)
-    # 3. return dictionary
     return procedure_info
 
-# called in response to client request for a procedure update
-# client provides list of procedure_ids that must be updated
+
+# need to add each record to a json object and send it
 def get_procedure_update(procedure_id_lst):
-    # need to add each record to a json object and send it
+    """
+    Called in response to client request for procedures to be synced
+
+    :param procedure_id_lst: List of procedure IDs that need to be updated on client
+    :type procedure_id_lst:
+    :return: Not defined yet
+    """
+
     for proc_id in procedure_id_lst:
         record = db(revisions_table.procedure_id == proc_table.proc_id).select(proc_table.procedure_id,
                                                                                  proc_table.user_id,
