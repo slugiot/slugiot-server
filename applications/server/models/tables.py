7f3dfb2c1241f6620d8d3b796467b8a9fa2bc165
Allison Hume <arhume@ucsc.edu>
2016-04-26 12:55:06 -0700

    merge caused duplicated tables for some reason - removed old ones

--- applications/server/models/tables.py
+++ applications/server/models/tables.py
@@ -41,22 +41,6 @@ db.define_table('runs_on',
                 Field('procedure_name'), # Name of the procedure on a particular device.
                 )
 
-# with this new split table definition it makes sense to just use the automatic id in this table as the procedure_id
-db.define_table('procedures',
-                # TODO: add device_id or account or something.
-                #Field('procedure_id', 'bigint', required=True),  # key
-                Field('user_email', 'string', required=True),
-                Field('name', 'string')  # Name of procedure
-                )
-
-db.define_table('procedure_revisions',
-                Field('procedure_id', 'bigint', required=True),  # key
-                Field('procedure_data', 'text', required=True),  # Actual code for procedure - is check IS_LENGTH(65536) ok?
-                # Otherwise use string and specifiy larger length
-                Field('last_update', 'datetime', default=datetime.datetime.utcnow(), required=True),
-                Field('stable_version', 'boolean', required=True) # True for stable False for not stable
-                )
-
 ##############
 # Permission table.
 
