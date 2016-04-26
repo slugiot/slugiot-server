c3412eee51a0c3811aecb458e53e1231d8fe14b9
Allison Hume <arhume@ucsc.edu>
2016-04-26 12:44:37 -0700

    fixing some small issues to start testing

--- applications/server/modules/test_proc_harness.py
+++ applications/server/modules/test_proc_harness.py
@@ -1,4 +1,5 @@
 import proc_harness_module as phm
+from gluon import current
 
 db = current.db
 proc_table = db.procedures
@@ -16,16 +17,16 @@ def run_test():
 
     phm.save(proc_id2, "blahblah3", False)
 
-    # for row in db(proc_table).select(): 
-    #    print row.id, row.user_id, row.name  
-    # #for row in db(revisions_table).select(): 
+    #for row in db(proc_table).select():
+    #    print row.id, row.user_id, row.name
+    #for row in db(revisions_table).select():
     #    print row.procedure_id, row.procedure_data, row.last_update, row.stable_version
 
     proc_list1 = phm.get_procedures_for_user("blah@blah")
     proc_list2 = phm.get_procedures_for_user("blah2@blah")
 
-    for row in db(revisions_table).select():  
-    print row.procedure_id, row.procedure_data, row.last_update, row.stable_version
+    for row in db(revisions_table).select():
+        print row.procedure_id, row.procedure_data, row.last_update, row.stable_version
 
     print "first", phm.get_procedure_data(proc_list1[0], True)
     print "second", phm.get_procedure_data(proc_list2[0], False)
