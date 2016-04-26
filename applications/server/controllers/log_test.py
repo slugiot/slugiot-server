69558592955fa2575675d72258ee5b118096d509
RobinChen <chienpinchen@gmail.com>
2016-04-25 18:25:32 -0700

    Add comments according to python formatting conventions and add testing csv file.

--- applications/server/controllers/log_test.py
+++ applications/server/controllers/log_test.py
@@ -1,4 +1,10 @@
-# tesing logs database
+# -*- coding: utf-8 -*-
+""" log_test.py
+    Python Code for CMPS 276 Project
+    This file is used to test logs database.
+    First, it will generate some fake data for log table, output table.
+    Then, log_report function will construct the form for visualizing in View.
+"""
 
 # add random log data by populate
 from gluon.contrib.populate import populate
@@ -23,15 +29,29 @@ populate(db.Living_room, 100)
 k_temp = db().select(db.Kitchen.Temperature)
 l_temp = db().select(db.Living_room.Temperature)
 
+
 def log_report():
+    """
+    This function, now, use fake database of log and output to generate
+    SQLFORM for log and save output to .csv for dygraph in view.
+
+    :return: Dict with the SQLFORM for log data.
+    :rtype: dict
+
+    """
+    # Construct log data into SQLFORM
     grid = SQLFORM.grid(db.logs, deletable=False, editable=False,
-                             create=False, paginate=50, formstyle='table3cols')
-    # write fake temperature data to a csv file
+                        create=False, paginate=50, formstyle='table3cols')
+
+    # Write fake temperature data to a csv file
     import csv
-    with open('./applications/server/static/temper.csv', 'wb') as csvfile:
+    import os
+    m_path = os.getcwd()
+    tot_path = os.path.join(m_path, 'applications', 'server', 'static', 'temper.csv')
+    with open(tot_path, 'wb') as csvfile:
         tmpwriter = csv.writer(csvfile, delimiter=',')
-        tmpwriter.writerow(['data','Kitchen','Living_room'])
+        tmpwriter.writerow(['data', 'Kitchen', 'Living_room'])
         for i in range(len(k_temp)):
-            tmpwriter.writerow([str(i),str(k_temp[i].Temperature),str(l_temp[i].Temperature)])
+            tmpwriter.writerow([str(i), str(k_temp[i].Temperature), str(l_temp[i].Temperature)])
 
     return dict(grid=grid)
