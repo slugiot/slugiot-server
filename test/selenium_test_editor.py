beca24528a759040cd4aedb09a216105c28a0737
Han Bai <baihan6@gmail.com>
2016-05-08 21:23:30 -0700

    fix bugs, wait for procedure to be saved before do the assertion check

--- test/selenium_test_editor.py
+++ test/selenium_test_editor.py
@@ -8,29 +8,33 @@ from selenium.webdriver.common.keys import Keys
 # so that can tell unittest module this is a test case
 class EditorTest(unittest.TestCase):
 
-	# get called before every test function in this class
+    # get called before every test function in this class
     def setUp(self):
-    	# create instance of Firefox WebDriver
+        # create instance of Firefox WebDriver
         self.driver = webdriver.Firefox()
 
     def test_temp_save(self):
-    	# local reference to the driver object
+        # local reference to the driver object
         driver = self.driver
         driver.get("http://localhost:8000/editor/test_edit?procedure_id=1&stable=false")
         time.sleep(2)
         # locate the temporary save button
         element = driver.find_element_by_class_name("btn-primary")
         element.click()
+        # wait until saved
+        time.sleep(2)
         self.assertIn("file saved successfully", driver.page_source)
 
     def test_stable_save_success(self):
-    	driver = self.driver
-    	driver.get("http://localhost:8000/editor/test_edit?procedure_id=1&stable=false")
-    	time.sleep(2)
-    	# locate the stable save button
-    	element = driver.find_element_by_class_name("btn-info")
-    	element.click()
-    	self.assertIn("file saved successfully", driver.page_source)
+        driver = self.driver
+        driver.get("http://localhost:8000/editor/test_edit?procedure_id=1&stable=false")
+        time.sleep(2)
+        # locate the stable save button
+        element = driver.find_element_by_class_name("btn-info")
+        element.click()
+        # wait until saved
+        time.sleep(2)
+        self.assertIn("file saved successfully", driver.page_source)
 
     # get called after every test method
     def tearDown(self):
