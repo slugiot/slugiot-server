8055814c6de7d8fb0e226846563ec536f757f9e3
Han Bai <baihan6@gmail.com>
2016-05-08 21:27:25 -0700

    add function for stable save failed test case

--- test/selenium_test_editor.py
+++ test/selenium_test_editor.py
@@ -36,6 +36,24 @@ class EditorTest(unittest.TestCase):
         time.sleep(2)
         self.assertIn("file saved successfully", driver.page_source)
 
+    def test_stable_save_fail(self):
+        driver = self.driver
+        driver.get("http://localhost:8000/editor/test_edit?procedure_id=1&stable=false")
+        time.sleep(2)
+
+        # TODO:
+        # locate the editor area
+        # use .send_keys("") method add some code in editor area
+        # add syntax error code intentionally
+
+        # locate the stable save button
+        element = driver.find_element_by_class_name("btn-info")
+        element.click()
+        # wait until saved
+        time.sleep(2)
+        self.assertIn("SyntaxError", driver.page_source)
+
+
     # get called after every test method
     def tearDown(self):
         self.driver.close()
