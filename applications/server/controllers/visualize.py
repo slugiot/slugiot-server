7e6a976ea11cfdff935c6b10499bfddc5d1e2a43
liujunlovecs <liujunlovecs@gmail.com>
2016-05-11 21:48:24 -0700

    add ajax request to server and modify the get_data method

--- applications/server/controllers/visualize.py
+++ applications/server/controllers/visualize.py
@@ -15,20 +15,45 @@ def test_fill():
     db(db.outputs).delete()
     db(db.logs).delete()
 
+    # fill some data for module_values table
+    # db(db.module_values).delete()
+    # db.module_values.insert(device_id=device_id,
+    #                         modulename="egg",
+    #                         name="egg",
+    #                         output_value="egg"
+    #                         )
+    # db.module_values.insert(device_id=device_id,
+    #                         modulename="leg",
+    #                         name="leg",
+    #                         output_value="leg"
+    #                         )
+
     # Let us insert some new random data.
     now = datetime.datetime.utcnow()
     for i in range(3):
         db.outputs.insert(device_id=device_id,
-                          modulename=module,
+                          procedure_id=module,
                           name=out_var,
-                          time_stamp=now - datetime.timedelta(hours=i),
-                          output_value=random.random(),
+                          output_time_stamp=now - datetime.timedelta(hours=i),
+                          output_value=random.random() * 20,
                           tag="1")
         db.logs.insert(device_id=device_id,
                        modulename=module,
-                       time_stamp=now - datetime.timedelta(hours=i),
+                       logged_time_stamp=now - datetime.timedelta(hours=i),
                        log_level=log_level,
                        log_message='This is message' + str(i) + '.')
+
+
+# @auth.requires_signature()
+def get_modulename():
+    device_id = request.vars.device_id
+    modulename = []
+    for row in db(db.module_values.device_id == device_id).select():
+        modulename.append(row.modulename)
+    print modulename
+    print "end of get_modulename"
+    result = {'module_name':modulename}
+    return response.json(result)
 
 
 # @auth.requires_signature()
@@ -38,137 +63,16 @@ def get_data():
     device_id, module_name, name, date1, date2
     ]
     """
-
-    # result = {
-    #     'mixed_data': [
-    #         {
-    #             'type': 'output',
-    #             'deivce_id': 'home',
-    #             'modulename': 'batman',
-    #             'time_stamp': '2016-04-01',
-    #             'content': 'name: temperature, value: 22, tag: 1'
-    #         },
-    #         {
-    #             'type': 'log',
-    #             'deivce_id': 'office',
-    #             'modulename': 'superman',
-    #             'time_stamp': '2016-04-02',
-    #             'content': 'name: normal log, value: Your system went smoothly'
-    #         },
-    #         {
-    #             'type': 'output',
-    #             'deivce_id': 'office',
-    #             'modulename': 'batman',
-    #             'time_stamp': '2016-04-03',
-    #             'content': 'name: temperature, value: 21, tag: 1'
-    #         },
-    #         {
-    #             'type': 'log',
-    #             'deivce_id': 'scholl',
-    #             'modulename': 'superman',
-    #             'time_stamp': '2016-04-04',
-    #             'content': 'name: normal log, value: administrator logged in'
-    #         }
-    #     ],
-    #     'output_data': [
-    #         {
-    #             'deivce_id': 'home',
-    #             'modulename': 'batman',
-    #             'name': 'temperature',
-    #             'output_value': 22,
-    #             'tag': '1',
-    #             'time_stamp': '2016-04-01'
-    #         },
-    #         {
-    #             'deivce_id': 'office',
-    #             'modulename': 'batman',
-    #             'name': 'temperature',
-    #             'output_value': 26,
-    #             'tag': '1',
-    #             'time_stamp': '2016-04-02'
-    #         },
-    #         {
-    #             'deivce_id': 'home',
-    #             'modulename': 'batman',
-    #             'name': 'temperature',
-    #             'output_value': 20,
-    #             'tag': '1',
-    #             'time_stamp': '2016-04-03'
-    #         },
-    #         {
-    #             'deivce_id': 'office',
-    #             'modulename': 'batman',
-    #             'name': 'temperature',
-    #             'output_value': 23,
-    #             'tag': '1',
-    #             'time_stamp': '2016-04-04'
-    #         }],
-    #     'log_data': [
-    #         {
-    #             'device_id': 'office',
-    #             'modulename': 'superman',
-    #             'name': 'normal log',
-    #             'output_value': 'Your system went smoothly',
-    #             'time_stamp': '2016-04-01',
-    #             'log_level': '0'
-    #         },
-    #         {
-    #             'device_id': 'home',
-    #             'modulename': 'superman',
-    #             'name': 'normal log',
-    #             'output_value': 'Your system just got modified',
-    #             'time_stamp': '2016-04-02',
-    #             'log_level': '0'
-    #         },
-    #         {
-    #             'device_id': 'office',
-    #             'modulename': 'superman',
-    #             'name': 'critical log',
-    #             'output_value': 'Unauthorized logged in',
-    #             'time_stamp': '2016-04-03',
-    #             'log_level': '4'
-    #         },
-    #         {
-    #             'device_id': 'home',
-    #             'modulename': 'superman',
-    #             'name': 'normal log',
-    #             'output_value': 'guest logged in',
-    #             'time_stamp': '2016-04-04',
-    #             'log_level': '0'
-    #         }
-    #     ]
-    # }
-    # Read chapter 3 of web2py book.  Or maybe
-    # device_id = request.args(0)
-    # module = request.args(1)
-    # out_var = request.args(2)
-    # date1 = datetime.parsedate(request.args(3))
-    # date2 = datetime.parsedate(request.args(4))
-    # if (device_id is None or module is None):
-    #     raise HTTP(500)
-    # for row in db((db.outputs.device_id == device_id) &
-    #                       (db.outputs.modulename == module) &
-    #                       (db.outputs.name == out_var) &
-    #                       (db.output.timesteamp <= date2) &
-    #                       (db.output.timestamp >= date1)).select():
-    #     result.append(dict(d=row.timestamp.isoformat(), v=row.value))
+    print 1111111111111
     test_fill()
-    result = []
-    print 111
-    print 1
-    # dd = db(db.outpts).select()
-    print 3
+    print 2222222222222
 
-    # s = request.get('start')
-    # e = request.get('end')
-    #
-    # print s
-    # print e
     s = request.vars.start
     e = request.vars.end
 
     print s
     # start = datetime.strptime("2016-05-03 21:20:20", "%Y-%m-%d %H:%M:%S")
+    # parse the start date and end date
     start_year = int(s[0:4])
     start_month = int(s[5:7])
     start_day = int(s[8:10])
@@ -183,73 +87,46 @@ def get_data():
     end_minute = int(e[14:16])
     end_second = int(e[17:19])
 
+    # transform the start date and end date into datetime format
     start = datetime.datetime(start_year, start_month, start_day, start_hour, start_minute, start_second)
     end = datetime.datetime(end_year, end_month, end_day, end_hour, end_minute, end_second)
-    print start
-    print end
-    print s
 
-    output_data = db((db.outputs.time_stamp >= start) &
-                     (db.outputs.time_stamp <= end)).select()
-    log_data = db((db.logs.time_stamp >= start) &
-                  (db.logs.time_stamp <= end)).select()
+    output_data = db((db.outputs.output_time_stamp >= start) &
+                     (db.outputs.output_time_stamp <= end)).select()
+    log_data = db((db.logs.logged_time_stamp >= start) &
+                  (db.logs.logged_time_stamp <= end)).select()
     mixed_data = []
 
-    for row in db((db.outputs.time_stamp >= start) & (db.outputs.time_stamp <= end)).select(orderby=db.outputs.time_stamp):
+
+    print 'begin of output_data reading'
+    for row in db((db.outputs.output_time_stamp >= start) & (db.outputs.output_time_stamp <= end)).select(orderby=db.outputs.output_time_stamp):
         type = 'output'
         device_id = row.device_id
-        modulename = row.modulename
-        time_stamp = row.time_stamp
+        modulename = row.procedure_id
+        time_stamp = row.output_time_stamp
         name = row.name
         value = row.output_value
         tag = row.tag
         content = 'name: ' + str(name) + ', value: ' + str(value) + ', tag: ' + str(tag)
-        mixed_data.append({'type': type, 'device_id': device_id, 'modulename': modulename, 'time_stamp': time_stamp,
-                           'content': content})
-        print "before mixed"
-        # db.mixed_data.insert(device_type=type,
-        #                      device_id=device_id,
-        #                      modulename=modulename,
-        #                      time_stamp=time_stamp,
-        #                      device_content=content
-        #                      )
-        #             'type': 'log',
-        #             'deivce_id': 'scholl',
-        #             'modulename': 'superman',
-        #             'time_stamp': '2016-04-04',
-        #             'content': 'name: normal log, value: administrator logged in'
+        mixed_data.append({'type': type, 'device_id': device_id, 'modulename': modulename, 'time_stamp': time_stamp,'content': content})
+    print 'end of output_data reading'
     # add log_data in
-    for row in db((db.logs.time_stamp >= start) & (db.logs.time_stamp <= end)).select(orderby=db.logs.time_stamp):
+    for row in db((db.logs.logged_time_stamp >= start) & (db.logs.logged_time_stamp <= end)).select(orderby=db.logs.logged_time_stamp):
         type = 'log'
         device_id = row.device_id
         modulename = row.modulename
-        time_stamp = row.time_stamp
+        time_stamp = row.logged_time_stamp
         log_level = row.log_level
         log_message = row.log_message
         content = 'name: ' + str(log_level) + ', value: ' + str(log_message)
         mixed_data.append({'type': type, 'device_id': device_id, 'modulename': modulename, 'time_stamp': time_stamp,
                            'content': content})
-        # db.mixed_data.insert(device_type=type,
-        #                      device_id=device_id,
-        #                      modulename=modulename,
-        #                      time_stamp=time_stamp,
-        #                      device_content=content
-        #                      )
-    # for row in db(db.mixed_data).select(orderby=db.mixed_data.time_stamp):
-    #     type = row.device_type
-    #     device_id = row.device_id
-    #     modulename = row.modulename
-    #     time_stamp = row.time_stamp
-    #     content = row.device_content
-    #     mixed_data.append({'type': type, 'device_id': device_id, 'modulename': modulename, 'time_stamp': time_stamp,
-    #                        'content': content})
-    # print log_data
-    print 222
+
+
+    print 'end of log_data reading'
     # sorted(mixed_data, key=mixed_data.time_stamp, reverse=False)
     mixed_data.sort(key=lambda r: r['time_stamp'])
-    # print "the size of the mixed_data is " + str(mixed_data.count())
-    print mixed_data
-    print 333
+
     result = {'output_data': output_data, 'log_data': log_data, 'mixed_data': mixed_data}
 
     return response.json(result)
