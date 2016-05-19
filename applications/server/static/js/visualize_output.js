b83229a89d2136edea923d87b4f6c0fa679e9c62
RobinChen <chienpinchen@gmail.com>
2016-05-19 14:30:56 -0700

    Add bar chart and can swtich by tab.

--- applications/server/static/js/visualize_output.js
+++ applications/server/static/js/visualize_output.js
@@ -55,6 +55,7 @@
     });
 
     // ------------  Dygraph testing  ------------
+    // Extra plotting style
     function barChartPlotter(e) {
         var ctx = e.drawingContext;
         var points = e.points;
@@ -72,8 +73,61 @@
             ctx.fillRect(center_x - bar_width / 2, p.canvasy, bar_width, y_bottom - p.canvasy);
             ctx.strokeRect(center_x - bar_width / 2, p.canvasy, bar_width, y_bottom - p.canvasy);
         }
-    }
-    ;
+    };
+
+    function multiColumnBarPlotter(e) {
+        // We need to handle all the series simultaneously.
+        if (e.seriesIndex !== 0) return;
+
+        var g = e.dygraph;
+        var ctx = e.drawingContext;
+        var sets = e.allSeriesPoints;
+        var y_bottom = e.dygraph.toDomYCoord(0);
+
+        // Find the minimum separation between x-values.
+        // This determines the bar width.
+        var min_sep = Infinity;
+        for (var j = 0; j < sets.length; j++) {
+            var points = sets[j];
+            for (var i = 1; i < points.length; i++) {
+              var sep = points[i].canvasx - points[i - 1].canvasx;
+              if (sep < min_sep) min_sep = sep;
+            }
+        }
+        var bar_width = Math.floor(2.0 / 3 * min_sep);
+
+        var fillColors = [];
+        var strokeColors = g.getColors();
+        for (var i = 0; i < strokeColors.length; i++) {
+            fillColors.push(darkenColor(strokeColors[i]));
+        }
+
+        for (var j = 0; j < sets.length; j++) {
+            ctx.fillStyle = fillColors[j];
+            ctx.strokeStyle = strokeColors[j];
+            for (var i = 0; i < sets[j].length; i++) {
+                var p = sets[j][i];
+                var center_x = p.canvasx;
+                var x_left = center_x - (bar_width / 2) * (1 - j/(sets.length-1));
+
+                ctx.fillRect(x_left, p.canvasy,
+                    bar_width/sets.length, y_bottom - p.canvasy);
+
+                ctx.strokeRect(x_left, p.canvasy,
+                    bar_width/sets.length, y_bottom - p.canvasy);
+            }
+        }
+    };
+
+    // Darken a color
+    function darkenColor(colorStr) {
+        // Defined in dygraph-utils.js
+        var color = Dygraph.toRGB_(colorStr);
+        color.r = Math.floor((255 + color.r) / 2);
+        color.g = Math.floor((255 + color.g) / 2);
+        color.b = Math.floor((255 + color.b) / 2);
+        return 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')';
+    };
 
     dy_plot = function () {
         var graph_com = document.getElementById("dygraph_target1");
@@ -173,6 +227,13 @@
 
         }
 
+    };
+
+    function line_plot(){
+        g2.updateOptions({plotter: DygraphCanvasRenderer._linePlotter, title: 'Temperature Line Chart'});
+    };
+    function bar_plot(){
+        g2.updateOptions({plotter: multiColumnBarPlotter, title: 'Temperature Bar Chart'});
     };
 
     dy_plot();
