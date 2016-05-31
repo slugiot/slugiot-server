383416f2dea1717872f3b1810a06eced689925dc
Han Bai <baihan6@gmail.com>
2016-05-30 23:07:12 -0700

    Revise temp & stable button of procedure list

--- applications/server/static/js/ajax_editor.js
+++ applications/server/static/js/ajax_editor.js
@@ -23,6 +23,7 @@ function load_procedure(url ,procedure_id, stable, device_id) {
                     // Load procedure list at left side
                     var procedure_list = '';
                     for (var i = 0; i < json['id_list'].length; i++) {
+
                         // generate the link tag
                         procedure_list += '<button type="button" class="list-group-item';
                         if (json['id'] == json['id_list'][i]) {
@@ -30,18 +31,24 @@ function load_procedure(url ,procedure_id, stable, device_id) {
                         }
                         procedure_list += '"';
                         procedure_list += 'id="' + json['id_list'][i] + '">';
+
                         // name of the procedure
                         procedure_list += json['name_list'][i];
-                        // button access to temporary saved procedure
-                        procedure_list += '<a href="?device_id=' + json['dev_id'] + '&procedure_id=' + json['id_list'][i] + '&stable=false">';
-                        var procedure_temp = '<span class="glyphicon glyphicon-edit"></span>';
-                        procedure_list += procedure_temp;
-                        procedure_list += '</a>';
+
                         // button access to stable saved procedure
                         procedure_list += '<a href="?device_id=' + json['dev_id'] + '&procedure_id=' + json['id_list'][i] + '&stable=true">';
-                        var procedure_temp = '<span class="glyphicon glyphicon-check"></span>';
+                        var procedure_temp = '<span class="label label-default label-pill pull-xs-right" style="float: right; margin-left: 2px">stable</span>';
+                        // var procedure_temp = '<span class="glyphicon glyphicon-check"></span>';
                         procedure_list += procedure_temp;
                         procedure_list += '</a>';
+
+                        // button access to temporary saved procedure
+                        procedure_list += '<a href="?device_id=' + json['dev_id'] + '&procedure_id=' + json['id_list'][i] + '&stable=false">';
+                        var procedure_temp = '<span class="label label-default label-pill pull-xs-right" style="float: right">temp</span>';
+                        // var procedure_temp = '<span class="glyphicon glyphicon-edit"></span>';
+                        procedure_list += procedure_temp;
+                        procedure_list += '</a>';
+
                         procedure_list += '</button>';
                     }
                     $('#procedures').append($(procedure_list));
